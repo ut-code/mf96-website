6ea62c07f5ed03921e1e7867c19bef00c0acccbf
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-04-05 13:50:17 +0900

    企画の説明を追加

--- app.vue
+++ app.vue
@@ -1,17 +1,22 @@
 <template>
-  <div>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6">
-        <v-card>
-          <v-card-title class="headline">
-            あなたのためのプログラミング
-          </v-card-title>
-          <!-- <v-card-text> </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-          </v-card-actions> -->
-        </v-card>
-      </v-col>
-    </v-row>
-  </div>
+  <v-app>
+    <v-app-bar>
+      <v-toolbar-title>ut.code(); 第96回五月祭 </v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-card>
+        <v-card-title class="headline">
+          あなたのためのプログラミング
+        </v-card-title>
+        <v-card-text>
+          <p>ut.code(); の第96回五月祭企画へようこそ！</p>
+          <p>
+            「あなたのためのプログラミング」は、皆さま全員に向けた企画です。<br />
+            プログラマーの方も、プログラミングをやったことがないという方も、お楽しみいただけます。<br />
+            お子さまも、親御さんも、学生さんも、社会人の方も、お楽しみいただけます。
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-main>
+  </v-app>
 </template>
