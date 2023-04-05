0f273de299820828f1b9577035d2c35e2086b790
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-04-05 13:51:54 +0900

    レイアウト調整

--- app.vue
+++ app.vue
@@ -4,19 +4,25 @@
       <v-toolbar-title>ut.code(); 第96回五月祭 </v-toolbar-title>
     </v-app-bar>
     <v-main>
-      <v-card>
-        <v-card-title class="headline">
-          あなたのためのプログラミング
-        </v-card-title>
-        <v-card-text>
-          <p>ut.code(); の第96回五月祭企画へようこそ！</p>
-          <p>
-            「あなたのためのプログラミング」は、皆さま全員に向けた企画です。<br />
-            プログラマーの方も、プログラミングをやったことがないという方も、お楽しみいただけます。<br />
-            お子さまも、親御さんも、学生さんも、社会人の方も、お楽しみいただけます。
-          </p>
-        </v-card-text>
-      </v-card>
+      <div class="py-8 px-6">
+        <v-row justify="center" align="center">
+          <v-col cols="16" sm="10" md="8">
+            <v-card>
+              <v-card-title class="headline">
+                あなたのためのプログラミング
+              </v-card-title>
+              <v-card-text>
+                <p>ut.code(); の第96回五月祭企画へようこそ！</p>
+                <p>
+                  「あなたのためのプログラミング」は、皆さま全員に向けた企画です。<br />
+                  プログラマーの方も、プログラミングをやったことがないという方も、お楽しみいただけます。<br />
+                  お子さまも、親御さんも、学生さんも、社会人の方も、お楽しみいただけます。
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
     </v-main>
   </v-app>
 </template>
