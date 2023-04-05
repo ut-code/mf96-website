0b242c1cf78f9783b046fedfda1b9728b2c47b46
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-04-05 11:54:03 +0900

    vuetifyのテスト

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title class="headline">
+            あなたのためのプログラミング
+          </v-card-title>
+          <!-- <v-card-text> </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+          </v-card-actions> -->
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
